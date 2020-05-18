8d6f84c81909311b0c7f6a1dbb0d70927c085cbb
xduTd <973958891@qq.com>
2020-05-19 03:36:46 +0800

    startEdition

---
+++ Account.java
@@ -0,0 +1,16 @@
+package BFICheck;
+
+/*
+ * 输入个人信息（创建账户时） 核验
+ * 读取tag
+ * 获取上一个block的hash
+ * 读取上一个block的信息
+ * 输出（验证）
+ * 创建起始block
+ * 创建中间的block
+ * 创建临时不可读取hash的block
+ */
+
+public class Account {
+	
+}
