da00cc4765f9ec11f7342146373a450a009395e8
xduTd <973958891@qq.com>
2020-05-19 18:42:51 +0800

    first updated by TD

--- BFICUtilities.java
+++ BFICUtilities.java
@@ -1,6 +1,19 @@
 package BFICheck;
 
+import java.util.Scanner;
+
 //This Class only contains utility functions
 public class BFICUtilities {
+	// TODO: prompting and scan a valid order to return
+	public static String scanString(Scanner scan, String prompt) {
+		String str = null;
+		
+		System.out.println(prompt);
+		do {
+			str = scan.nextLine();
+		} while (!str.isBlank());
+		return str;
+	}
+	
 	
 }
